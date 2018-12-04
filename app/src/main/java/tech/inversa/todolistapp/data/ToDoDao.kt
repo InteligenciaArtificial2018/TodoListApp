f6150a9dcb0022b66a993851524cfcee1bafaa84
Hector Sabillon <h.hsabillon@gmail.com>
2018-12-04 14:52:12 -0600

    Creación de los layouts de la App

--- app/src/main/java/tech/inversa/todolistapp/data/ToDoDao.kt
+++ app/src/main/java/tech/inversa/todolistapp/data/ToDoDao.kt
@@ -1,7 +1,6 @@
 package tech.inversa.todolistapp.data
 
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface ToDoDao {
@@ -11,7 +10,31 @@ interface ToDoDao {
     @Query("SELECT * FROM todo ORDER BY id ASC")
     fun getTodoList(): List<Todo>
 
+    /**
+     * Retorna una tupla desde la tabla todo
+     * @param id el valor de la llave primaria a retornar.
+     */
     @Query("SELECT * FROM todo WHERE id = :id")
     fun getTodoItem(id: Int): Todo
 
+    /**
+     * Inserta una nueva tupla en la tabla todo.
+     * @param todo la tupla a insertar en la tabla.
+     */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun saveTodo(todo: Todo)
+
+    /**
+     * Actualiza una tupla en la tabla todo.
+     * @param todo el valor de la tupla a actualizar.
+     */
+    @Update
+    fun updateTodo(todo: Todo)
+
+    /**
+     * Remueve una tupla de la tabla todo.
+     * @param todo el valor de la tupla a remover.
+     */
+    @Delete
+    fun deleteTodo(todo: Todo)
 }
